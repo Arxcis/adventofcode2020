5476591f32c6ef7e18769bd3ddd117277f2240b4
Jonas Johan Solsvik <jonasjso@protonmail.com>
2020-12-01 21:54:20 +0100

    Rename py.sh -> python.sh

--- days/day-00-example/test.sh
+++ days/day-00-example/test.sh
@@ -12,7 +12,7 @@ $D/../../languages/go.sh    $D/input.txt $D/output.txt $D/solutions/example.go
 $D/../../languages/java.sh  $D/input.txt $D/output.txt $D/solutions Example
 $D/../../languages/node.sh  $D/input.txt $D/output.txt $D/solutions/example.node.mjs
 $D/../../languages/php.sh   $D/input.txt $D/output.txt $D/solutions/example.php
-$D/../../languages/py.sh    $D/input.txt $D/output.txt $D/solutions/example.py
+$D/../../languages/python.sh    $D/input.txt $D/output.txt $D/solutions/example.py
 $D/../../languages/sml.sh   $D/input.txt $D/output.txt $D/solutions/example.sml
 $D/../../languages/rust.sh  $D/input.txt $D/output.txt $D/solutions/example.rs
 $D/../../languages/ruby.sh  $D/input.txt $D/output.txt $D/solutions/example.rb
