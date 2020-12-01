954b1e1e5e5337b978bc5f9333dae35a2a99f38a
Jonas Johan Solsvik <jonasjso@protonmail.com>
2020-12-01 20:11:50 +0100

    Trigger CI

--- days/day-01/solutions/day01.py
+++ days/day-01/solutions/day01.py
@@ -11,11 +11,11 @@ part2 = None
 for i in lines:
   for j in lines:
     if part1 is None and i + j == 2020:
-      part1 = i*j
+      part1 = i * j
 
     for k in lines:
       if part2 is None and i + j + k == 2020:
-        part2 = i*j*k
+        part2 = i * j * k
 
 print(part1)
 print(part2)
