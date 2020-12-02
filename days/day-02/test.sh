7de252144f9a204caaa440bd285c15b437bbbf96
Celebrian <per_kb@hotmail.com>
2020-12-02 21:22:29 +0100

    Added C++ solution to the test for day 2

--- days/day-02/test.sh
+++ days/day-02/test.sh
@@ -6,4 +6,5 @@ D=$(dirname $(realpath $0))
 echo ""
 echo "--- Day 2: Password Philosophy ---"
 $D/../../languages/c.sh $D/input.txt $D/output.txt $D/solutions/day02.c
+$D/../../languages/c.sh $D/input.txt $D/output.txt $D/solutions/day02.cpp
 echo ""
