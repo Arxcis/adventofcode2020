5d427aa3e58fc8247e3b82cf91805261cfc4d115
Jonas Johan Solsvik <jonasjso@protonmail.com>
2020-12-02 06:26:28 +0100

    solve day 02 in c

--- days/day-02/solutions/day02.c
+++ days/day-02/solutions/day02.c
@@ -1,13 +1,45 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
   const int MAX_LINE = 100;
 
+  int valid_passwords = 0;
+  int valid_passwords_part2 = 0;
   for (char line[MAX_LINE]; fgets(line, MAX_LINE, stdin); ) {
-    int num = 0;
-    sscanf(line, "%d", &num);
-    printf("%d", num);
+    int from = 0;
+    int to = 0;
+    char letter = 0;
+    char password[30];
+    sscanf(line, "%d-%d %c: %s", &from, &to, &letter, password);
+
+    int letter_occurances = 0;
+    for (int i = 0; i < strlen(password); i++) {
+      if (password[i] == letter) {
+        letter_occurances += 1;
+      }
+    }
+
+    if (letter_occurances >= from && letter_occurances <= to) {
+      valid_passwords += 1;
+    }
+
+    int letter_occurances_part2 = 0;
+    if (password[from-1] == letter) {
+      letter_occurances_part2 += 1;
+    }
+
+    if (password[to-1] == letter) {
+      letter_occurances_part2 += 1;
+    }
+
+    if (letter_occurances_part2 == 1) {
+      valid_passwords_part2 += 1;
+    }
   }
+
+  printf("%d\n", valid_passwords);
+  printf("%d\n", valid_passwords_part2);
 
   return 0;
 }
--- days/day-02/test.sh
+++ days/day-02/test.sh
@@ -4,6 +4,6 @@ set -e
 D=$(dirname $(realpath $0))
 
 echo ""
-echo "--- Day 2: ? ---"
+echo "--- Day 2: Password Philosophy ---"
 $D/../../languages/c.sh $D/input.txt $D/output.txt $D/solutions/day02.c
 echo ""
