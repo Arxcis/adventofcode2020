897789ec5fc9ee0cae2fda4c36272bcd8ea3663e
avokadoen <akselhallajs@gmail.com>
2020-12-03 12:19:00 +0100

    More idiomatic zig parsing

--- days/day-01/solutions/day01.zig
+++ days/day-01/solutions/day01.zig
@@ -1,29 +1,17 @@
 const std = @import("std");
+const mem = std.mem;
 const io = std.io;
 const ArrayList = std.ArrayList;
 const Allocator = std.mem.Allocator;
 
- // TODO: find std function to do this for me :(
-fn bytesToIntArrayList(allocator: *Allocator, bytes: []u8, length: usize) anyerror!ArrayList(u32) {
-    var list = ArrayList(u32).init(allocator);
-    try list.ensureCapacity(200);
+fn bytesToIntArrayList(allocator: *Allocator, bytes: []u8) !ArrayList(u32) {
+    var list = try ArrayList(u32).initCapacity(allocator, 200);
 
-    var i: usize = 0;
-    var stride: usize = 0;
-    while  (i < length) : (i += stride + 2) {
-        var j: usize = 0;
-        stride = 0;
-
-        while (j < bytes.len and bytes[i + j] != '\n') : (j += 1) {
-            stride = j;
-        }
-        
-        if (std.fmt.parseInt(u32, bytes[i..i+stride], 10)) |value| {
-            // TODO: handle this instead.
-            try list.append(value);
-        } else |err| {
-            std.debug.warn("failed to parse int. error: {}, i: {}, stride: {}, slice content: {}", .{err, i, stride, bytes[i..i+stride]});
-        }
+    var it = mem.split(bytes, "\r\n");
+    while (it.next()) |line| {
+        if (line.len == 0) continue; // empty line
+        const value = try std.fmt.parseInt(u32, line, 10);
+        try list.append(value);
     }
 
     return list;
@@ -42,7 +30,7 @@ pub fn main() anyerror!void {
 
     const length = try in.readAll(&buf);
 
-    var input = try bytesToIntArrayList(allocator, &buf, length);
+    var input = try bytesToIntArrayList(allocator, buf[0..length]);
 
     // brute force solution as the byte parsing was enough of a challenge for now ...
     var part1: u32 = 0;
