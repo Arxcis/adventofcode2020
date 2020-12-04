61c7d207d06c4640e84074ab5f2666fe502fcd82
Jonas Johan Solsvik <jonasjso@protonmail.com>
2020-12-04 23:35:08 +0100

    Delete input.jonas.txt and output.jonas.txt

--- days/day-04/test.sh
+++ days/day-04/test.sh
@@ -5,7 +5,7 @@ D=$(dirname $(realpath $0))
 
 echo
 echo "--- Day 4: Passport Processing ---"
-$D/../../languages/c.sh   $D/input.jonas.txt $D/output.jonas.txt $D/solutions/day04.c
+$D/../../languages/c.sh      $D/input.txt $D/output.txt $D/solutions/day04.c
 $D/../../languages/python.sh $D/input.txt $D/output.txt $D/solutions/python_klyve.py
 $D/../../languages/ruby.sh   $D/input.txt $D/output.txt $D/solutions/ruby_klyve.rb
 echo
