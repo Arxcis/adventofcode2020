bdb533dc596d766e9542de76f75c05d701c95d6e
Halvor <stektpotet@gmail.com>
2020-12-04 03:48:35 +0100

    Updated go solution to read from stdio

--- days/day-01/solutions/day01.go
+++ days/day-01/solutions/day01.go
@@ -1,48 +1,46 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strconv"
+  "bufio"
+  "fmt"
+  "os"
+  "strconv"
 )
 
 func main() {
-	file, _ := os.Open("input")
-	defer file.Close()
+  var nums []int
+  reader := bufio.NewReader(os.Stdin)
 
-	var nums []int
+  scanner := bufio.NewScanner(reader)
+  for scanner.Scan() {
+    i, _ := strconv.Atoi(scanner.Text())
+    nums = append(nums, i)
+  }
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		i, _ := strconv.Atoi(scanner.Text())
-		nums = append(nums, i)
-	}
+  for i, a := range nums {
+    for j, b := range nums[i:] {
+      if i == i+j {
+        continue
+      }
+      if a+b == 2020 {
+        fmt.Println(a * b)
+      }
+    }
+  }
 
-	for i, a := range nums {
-		for j, b := range nums[i:] {
-			if i == i+j {
-				continue
-			}
-			if a+b == 2020 {
-				fmt.Println(a * b)
-			}
-		}
-	}
-
-	for i, a := range nums {
-		for j, b := range nums[i:] {
-			if i == i+j {
-				continue
-			}
-			for k, c := range nums[i+j:] {
-				if i == i+j+k {
-					continue
-				}
-				if a+b+c == 2020 {
-					fmt.Println(a * b * c)
-				}
-			}
-		}
-	}
+  for i, a := range nums {
+    for j, b := range nums[i:] {
+      if i == i+j {
+        continue
+      }
+      for k, c := range nums[i+j:] {
+        if j == j+k {
+          continue
+        }
+        if a+b+c == 2020 {
+          fmt.Println(a * b * c)
+        }
+      }
+    }
+  }
 }
