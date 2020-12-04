484609f2e195f5f6007bd16312d30c4cafbb9e2c
avokadoen <akselhj@stud.ntnu.no>
2020-12-04 22:44:45 +0100

    remove compile time from zig benchmark

--- days/day-01/test.sh
+++ days/day-01/test.sh
@@ -5,6 +5,7 @@ D=$(dirname $(realpath $0))
 
 echo ""
 echo "--- Day 1: Report Repair ---"
+$D/../../languages/zig.sh    $D/input.txt $D/output.txt $D/solutions/day01.zig
 $D/../../languages/sml.sh    $D/input.txt $D/output.txt $D/solutions/day01.sml
 $D/../../languages/go.sh     $D/input.txt $D/output.txt $D/solutions/day01.go
 $D/../../languages/python.sh $D/input.txt $D/output.txt $D/solutions/one-liner.py
@@ -12,5 +13,4 @@ $D/../../languages/cpp.sh    $D/input.txt $D/output.txt $D/solutions/day01.cpp
 $D/../../languages/python.sh $D/input.txt $D/output.txt $D/solutions/day01.py
 $D/../../languages/rust.sh   $D/input.txt $D/output.txt $D/solutions/day01.rs
 $D/../../languages/ruby.sh   $D/input.txt $D/output.txt $D/solutions/day01.rb
-$D/../../languages/zig.sh    $D/input.txt $D/output.txt $D/solutions/day01.zig
 echo ""
--- languages/zig.sh
+++ languages/zig.sh
@@ -7,10 +7,22 @@ set -e
 INPUT="$1"
 OUTPUT="$2"
 SOLUTION="$3"
+CURRENT_PATH="$(pwd)"
+SOLUTION_NAME="zig_solution"
+TEMP_SOURCE="/tmp/$SOLUTION_NAME.zig"
+TEMP_BIN="/tmp/$SOLUTION_NAME"
 
+
+
+# Output directory can be controlled with zig build scripts, but seemingly has no compiler flag for it
+# Should probably refactor this later as it is somewhat cumbersome ... 
+cp $SOLUTION $TEMP_SOURCE
+cd "/tmp/"
+zig build-exe $TEMP_SOURCE --name $SOLUTION_NAME
+cd $CURRENT_PATH
 
 start=$(($(date +%s%N)/1000000))
-cat $INPUT | zig run $SOLUTION -O ReleaseFast | diff - $OUTPUT
+cat $INPUT | $TEMP_BIN | diff - $OUTPUT
 end=$(($(date +%s%N)/1000000))
 
 TIME="$(expr $end - $start)"
