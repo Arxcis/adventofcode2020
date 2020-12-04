bbb243881edc2f44efbc19d967842beb5985aeac
Halvor <stektpotet@gmail.com>
2020-12-04 04:21:59 +0100

    Day02 solution in go

--- days/day-02/test.sh
+++ days/day-02/test.sh
@@ -5,6 +5,7 @@ D=$(dirname $(realpath $0))
 
 echo ""
 echo "--- Day 2: Password Philosophy ---"
+$D/../../languages/go.sh      $D/input.txt $D/output.txt $D/solutions/day02.go
 $D/../../languages/c.sh       $D/input.txt $D/output.txt $D/solutions/day02.c
 $D/../../languages/cpp.sh     $D/input.txt $D/output.txt $D/solutions/day02.cpp
 $D/../../languages/python.sh  $D/input.txt $D/output.txt $D/solutions/day02.py
