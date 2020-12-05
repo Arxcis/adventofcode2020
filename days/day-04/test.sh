01f2d5e4f50b0717da4a28dc453c9174b8675865
Jørgen Hanssen <jorkiz97@gmail.com>
2020-12-05 09:47:32 +0100

    Changed name of day04.py

--- days/day-04/test.sh
+++ days/day-04/test.sh
@@ -5,6 +5,6 @@ D=$(dirname $(realpath $0))
 
 echo ""
 echo "--- Day 4: Passport processing ---"
-$D/../../languages/python.sh $D/input.txt $D/output.txt $D/solutions/day04.py
+$D/../../languages/python.sh $D/input.txt $D/output.txt $D/solutions/hanssen.py
 $D/../../languages/python.sh $D/input.txt $D/output.txt $D/solutions/preng.py
 echo ""
