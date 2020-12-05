3080293259fa7b239d17b5dccf112e896a54472d
Jean Niklas L'orange <jeannikl@hypirion.com>
2020-12-05 18:25:09 +0100

    Add Standard ML to day05 test.sh

--- days/day-05/test.sh
+++ days/day-05/test.sh
@@ -5,5 +5,6 @@ D=$(dirname $(realpath $0))
 
 echo ""
 echo "--- Day 5: Boarding pass ---"
+$D/../../languages/sml.sh    $D/input.txt $D/output.txt $D/solutions/day05.sml
 $D/../../languages/python.sh $D/input.txt $D/output.txt $D/solutions/preng.py
 echo ""
