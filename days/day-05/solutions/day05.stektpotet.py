57bcad02fc2e86ed4f11b48e570487fe4b01d965
Halvor Smedås <stektpotet@gmail.com>
2020-12-05 23:18:30 +0100

    Made it a one-liner

--- days/day-05/solutions/day05.stektpotet.py
+++ days/day-05/solutions/day05.stektpotet.py
@@ -1,6 +1,2 @@
-import sys
-ids = {int(''.join('0' if c == 'F' or c == 'L' else '1' for c in k), 2) for k in sys.stdin.read().split('\n')[:-1]}
-print(max(ids))
-for i in range(min(ids), max(ids)):
-    if not i in ids:
-        print(i)
+import sys; [(print(max(ids)), print(i)) for ids in [{int(''.join('0' if c == 'F' or c == 'L' else '1' for c in k), 2) for k in sys.stdin.read().split('\n')[:-1]}] for i in range(min(ids), max(ids)) if not i in ids]
+
