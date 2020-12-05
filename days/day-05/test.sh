e8c289199c816d65a2b59678b5292ec2b78fbb43
Bjarte Klyve Larsen <bjarte@maritimeoptima.com>
2020-12-05 18:27:52 +0100

    added a naive solution to day 5

--- days/day-05/test.sh
+++ days/day-05/test.sh
@@ -6,4 +6,5 @@ D=$(dirname $(realpath $0))
 echo ""
 echo "--- Day 5: Boarding pass ---"
 $D/../../languages/python.sh $D/input.txt $D/output.txt $D/solutions/preng.py
+$D/../../languages/python.sh $D/input.txt $D/output.txt $D/solutions/day05.klyve.py
 echo ""
