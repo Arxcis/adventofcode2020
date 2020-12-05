c3c5813f28d99fea569a3fe300f3c69b29c62e38
Jonas Johan Solsvik <jonasjso@protonmail.com>
2020-12-05 21:44:37 +0100

    rename day02.go files

--- days/day-02/test.sh
+++ days/day-02/test.sh
@@ -6,9 +6,9 @@ D=$(dirname $(realpath $0))
 echo ""
 echo "--- Day 2: Password Philosophy ---"
 $D/../../languages/c.sh       $D/input.txt $D/output.txt $D/solutions/day02.c
+$D/../../languages/go.sh      $D/input.txt $D/output.txt $D/solutions/day02.stektpotet.go
 $D/../../languages/cpp.sh     $D/input.txt $D/output.txt $D/solutions/day02.cpp
 $D/../../languages/sml.sh     $D/input.txt $D/output.txt $D/solutions/day02.sml
+$D/../../languages/go.sh      $D/input.txt $D/output.txt $D/solutions/day02.tholok97.go
 $D/../../languages/python.sh  $D/input.txt $D/output.txt $D/solutions/day02.py
-$D/../../languages/go.sh      $D/input.txt $D/output.txt $D/solutions/day02.go
-$D/../../languages/go.sh      $D/input.txt $D/output.txt $D/solutions/day02_tholok97.go
 echo ""
