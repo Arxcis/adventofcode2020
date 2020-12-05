4f2438ae34e1540515c4ec39668c313d828d6c34
tholok97 <thomas.lokkeborg@mimer.no>
2020-12-05 19:35:44 +0100

    day5 in Go
    This one was fun

--- days/day-05/test.sh
+++ days/day-05/test.sh
@@ -6,4 +6,5 @@ D=$(dirname $(realpath $0))
 echo ""
 echo "--- Day 5: Boarding pass ---"
 $D/../../languages/python.sh $D/input.txt $D/output.txt $D/solutions/preng.py
+$D/../../languages/go.sh $D/input.txt $D/output.txt $D/solutions/tholok97.go
 echo ""
