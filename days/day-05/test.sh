e2deb041921daba3e53c4715ca210fe312ab364c
Halvor Smedås <stektpotet@gmail.com>
2020-12-05 23:28:27 +0100

    Fixed yet another oops: Adding the tests for my solutions

--- days/day-05/test.sh
+++ days/day-05/test.sh
@@ -5,6 +5,8 @@ D=$(dirname $(realpath $0))
 
 echo ""
 echo "--- Day 5: Boarding pass ---"
+$D/../../languages/python.sh $D/input.txt $D/output.txt $D/solutions/day05.stektpotet.py
+$D/../../languages/python.sh $D/input.txt $D/output.txt $D/solutions/one-liner.stektpotet.py
 $D/../../languages/go.sh     $D/input.txt $D/output.txt $D/solutions/tholok97.go
 $D/../../languages/python.sh $D/input.txt $D/output.txt $D/solutions/day05.klyve.py
 $D/../../languages/sml.sh    $D/input.txt $D/output.txt $D/solutions/day05.sml
