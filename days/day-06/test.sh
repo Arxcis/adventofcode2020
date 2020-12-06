0b8714f86c59403c14ad7bf8becab3ebe72da439
Halvor Smedås <stektpotet@gmail.com>
2020-12-06 12:29:28 +0100

    Solution for day06 created!

--- days/day-06/test.sh
+++ days/day-06/test.sh
@@ -5,5 +5,6 @@ DIR=$(dirname $(realpath $0))
 
 echo ""
 echo "--- Day 6: Custom customs ---"
+$DIR/../../languages/python.sh $DIR/input.txt $DIR/output.txt $DIR/solutions/day06.stektpotet.py
 $DIR/../../languages/python.sh $DIR/input.txt $DIR/output.txt $DIR/solutions/day06.preng.py
 echo ""
