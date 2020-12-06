1e2699fe4af2868b11d0ba5cb8b202d67bbddaae
Halvor <stektpotet@gmail.com>
2020-12-07 00:41:04 +0100

    Added one-liner test to test.sh
    Turns out I hadn't added the one-liner to the test.sh before #57

--- days/day-06/test.sh
+++ days/day-06/test.sh
@@ -9,6 +9,7 @@ $D/../../languages/c.sh      $D/input.txt $D/output.txt $D/solutions/day06.c
 $D/../../languages/go.sh     $D/input.txt $D/output.txt $D/solutions/day06.stektpotet.go
 $D/../../languages/go.sh     $D/input.txt $D/output.txt $D/solutions/tholok97.go
 $D/../../languages/sml.sh    $D/input.txt $D/output.txt $D/solutions/day06.sml
+$D/../../languages/python.sh $D/input.txt $D/output.txt $D/solutions/one-liner.stektpotet.py
 $D/../../languages/python.sh $D/input.txt $D/output.txt $D/solutions/day06.stektpotet.py
 $D/../../languages/python.sh $D/input.txt $D/output.txt $D/solutions/day06.preng.py
 $D/../../languages/node.sh   $D/input.txt $D/output.txt $D/solutions/day06.mjs
