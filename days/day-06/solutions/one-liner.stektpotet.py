904394f3da06c6184a8c1a8a7a72fa68ddbc4e60
Halvor <stektpotet@gmail.com>
2020-12-07 00:48:22 +0100

    Revert "solved day06 in python one-liner"

--- days/day-06/solutions/one-liner.stektpotet.py
+++
@@ -1,1 +0,0 @@
-import sys; [print(sum(n)) for n in zip(*[(len(set(c for c in l if c != '\n')), len(set.intersection(*[set(c for c in g) for g in l.split()]))) for l in sys.stdin.read().split('\n\n')])]
