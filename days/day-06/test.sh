65f8c2ee710ab4bea0d206fe8155905b147568ec
Jean Niklas L'orange <jeannikl@hypirion.com>
2020-12-06 17:12:27 +0100

    Add Standard ML to test.sh

--- days/day-06/test.sh
+++ days/day-06/test.sh
@@ -5,5 +5,6 @@ DIR=$(dirname $(realpath $0))
 
 echo ""
 echo "--- Day 6: Custom customs ---"
+$DIR/../../languages/sml.sh    $DIR/input.txt $DIR/output.txt $DIR/solutions/day06.sml
 $DIR/../../languages/python.sh $DIR/input.txt $DIR/output.txt $DIR/solutions/day06.preng.py
 echo ""
