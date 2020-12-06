98534842f03366cdbab50ac6ff22e77fd4c2bfa7
Halvor <stektpotet@gmail.com>
2020-12-07 00:51:29 +0100

    Update test.sh

--- days/day-06/test.sh
+++ days/day-06/test.sh
@@ -5,6 +5,6 @@ DIR=$(dirname $(realpath $0))
 
 echo ""
 echo "--- Day 6: Custom customs ---"
-$DIR/../../languages/python.sh $DIR/input.txt $D/output.txt   $DIR/solutions/one-liner.stektpotet.py
+$DIR/../../languages/python.sh $DIR/input.txt $DIR/output.txt $DIR/solutions/one-liner.stektpotet.py
 $DIR/../../languages/python.sh $DIR/input.txt $DIR/output.txt $DIR/solutions/day06.preng.py
 echo ""
