e07e3bde8864d5f9cdbdd369167819eecb627dbb
Jonas Johan Solsvik <jonasjso@protonmail.com>
2020-12-08 19:08:59 +0100

    found a trivial solution expanding solutions AND io at the same time

--- languages/python.sh
+++ languages/python.sh
@@ -1,30 +1,25 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Usage:    ../../languages/python.sh  SOLUTION           INPUT/OUTPUT-pairs
+# Usage:    ../../languages/python.sh  "SOLUTIONS"        "INPUT/OUTPUT-pairs"
 #
-# Example:  ../../languages/python.sh  solutions/main.py  io/*
+# Example:  ../../languages/python.sh "solutions/*.py"   "io/*"
 # Expanded: ../../languages/python.sh  solutions/main.py  io/alice.input io/alice.output io/bob.input io/bob.output
 #
-SOLUTION="$1"
+SOLUTIONS=${1} # deliberitely expand arg
+IO=${2}        # deliberitely expand arg
 
-start=$(($(date +%s%N)/1000000))
 
-shift
-while (( "$#" >= 2 ))
-do
-	INPUT="$1"
-	OUTPUT="$2"
+for SOLUTION in $SOLUTIONS ; do
+  start=$(($(date +%s%N)/1000000))
 
-	cat $INPUT | python3 $SOLUTION | diff - $OUTPUT
+  # Pairwise iteration
+  while read INPUT OUTPUT; do
+	  cat $INPUT | python3 $SOLUTION | diff - $OUTPUT
+  done < <(echo $IO | xargs -n2)
 
-	shift;
-	shift;
+  end=$(($(date +%s%N)/1000000))
+  TIME="$(expr $end - $start)"
+  D=$(dirname $(realpath $0))
+  $D/../scripts/print-test.sh "python3" "$TIME" "$SOLUTION"
 done
-
-end=$(($(date +%s%N)/1000000))
-
-TIME="$(expr $end - $start)"
-
-D=$(dirname $(realpath $0))
-$D/../scripts/print-test.sh "python3" "$TIME" "$SOLUTION"
