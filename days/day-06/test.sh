16de8fdb1a290dbe8f11fd16b0216f16d4c05da4
Jonas Johan Solsvik <jonasjso@protonmail.com>
2020-12-08 23:22:57 +0100

    converted day06

--- days/day-06/test.sh
+++ days/day-06/test.sh
@@ -5,14 +5,12 @@ D=$(dirname $(realpath $0))
 
 echo ""
 echo "--- Day 6: Custom customs ---"
-$D/../../languages/c.sh      $D/input.txt $D/output.txt $D/solutions/day06.c
-$D/../../languages/go.sh     $D/input.txt $D/output.txt $D/solutions/day06.stektpotet.go
-$D/../../languages/go.sh     $D/input.txt $D/output.txt $D/solutions/tholok97.go
-$D/../../languages/python.sh $D/input.txt $D/output.txt $D/solutions/one-liner.stektpotet.py
-$D/../../languages/sml.sh    $D/input.txt $D/output.txt $D/solutions/day06.sml
-$D/../../languages/python.sh $D/input.txt $D/output.txt $D/solutions/one-liner.stektpotet.py
-$D/../../languages/python.sh $D/input.txt $D/output.txt $D/solutions/day06.stektpotet.py
-$D/../../languages/python.sh $D/input.txt $D/output.txt $D/solutions/day06.preng.py
-$D/../../languages/deno.sh   $D/input.txt $D/output.txt $D/solutions/day06.ts
-$D/../../languages/node.sh   $D/input.txt $D/output.txt $D/solutions/day06.mjs
+
+$D/../../languages/go.sh     "$D/solutions/*.go"   "$D/io/*"
+$D/../../languages/c.sh      "$D/solutions/*.c"    "$D/io/*"
+$D/../../languages/python.sh "$D/solutions/*.py"   "$D/io/*"
+$D/../../languages/sml.sh    "$D/solutions/*.sml"  "$D/io/*"
+$D/../../languages/deno.sh   "$D/solutions/*.ts"   "$D/io/*"
+$D/../../languages/node.sh   "$D/solutions/*.mjs"  "$D/io/*"
+
 echo ""
