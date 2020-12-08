30e79f83d29132afd862fbcec5bd1de78696abbb
Jonas Johan Solsvik <jonasjso@protonmail.com>
2020-12-08 23:56:56 +0100

    fix day 00 and day08

--- languages/bash.sh
+++ languages/bash.sh
@@ -1,19 +1,28 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Usage:   ./languages/bash.sh INPUT                 OUTPUT                 SOLUTION
-# Example: ./languages/bash.sh days/day-03/input.txt days/day-03/output.txt days/day-03/solutions/main.bash
+# Usage:      ../../languages/bash.sh  "SOLUTION_FILES"   "IO_FILES"
+#
+# Example:    ../../languages/bash.sh  "solutions/*.bash"   "io/*"
+# Expands to: ../../languages/bash.sh   solutions/main.bash  io/alice.input io/alice.output io/bob.input io/bob.output
+#
+SOLUTION_FILES=$1   # Expand FILES
+IO_FILES=$2         # Expand FILES
 
-INPUT="$1"
-OUTPUT="$2"
-SOLUTION="$3"
+for SOLUTION in $SOLUTION_FILES
+do
+  start=$(($(date +%s%N)/1000000))
 
-start=$(($(date +%s%N)/1000000))
-cat $INPUT | bash $SOLUTION | diff - $OUTPUT
-end=$(($(date +%s%N)/1000000))
+  # Pair-wise iteration
+  while read INPUT OUTPUT; do
+    cat $INPUT | bash $SOLUTION | diff - $OUTPUT
+  done < <(echo $IO_FILES | xargs -n2)
 
-TIME="$(expr $end - $start)"
+  end=$(($(date +%s%N)/1000000))
+
+  TIME="$(expr $end - $start)"
 
 
-D=$(dirname $(realpath $0))
-$D/../scripts/print-test.sh "bash" "$TIME" "$SOLUTION"
+  D=$(dirname $(realpath $0))
+  $D/../scripts/print-test.sh "bash" "$TIME" "$SOLUTION"
+done
--- languages/php.sh
+++ languages/php.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Usage:   ./languages/php.sh INPUT                 OUTPUT                 SOLUTION
-# Example: ./languages/php.sh days/day-03/input.txt days/day-03/output.txt days/day-03/solutions/main.php
+# Usage:      ../../languages/php.sh  "SOLUTION_FILES"   "IO_FILES"
+#
+# Example:    ../../languages/php.sh  "solutions/*.php"   "io/*"
+# Expands to: ../../languages/php.sh   solutions/main.php  io/alice.input io/alice.output io/bob.input io/bob.output
+#
+SOLUTION_FILES=$1   # Expand FILES
+IO_FILES=$2         # Expand FILES
 
-INPUT="$1"
-OUTPUT="$2"
-SOLUTION="$3"
+for SOLUTION in $SOLUTION_FILES
+do
+  start=$(($(date +%s%N)/1000000))
 
-start=$(($(date +%s%N)/1000000))
-cat $INPUT | php $SOLUTION | diff - $OUTPUT
-end=$(($(date +%s%N)/1000000))
+  # Pair-wise iteration
+  while read INPUT OUTPUT; do
+    cat $INPUT | php $SOLUTION | diff - $OUTPUT
+  done < <(echo $IO_FILES | xargs -n2)
 
-TIME="$(expr $end - $start)"
+  end=$(($(date +%s%N)/1000000))
 
-D=$(dirname $(realpath $0))
-$D/../scripts/print-test.sh "php" "$TIME" "$SOLUTION"
+  TIME="$(expr $end - $start)"
+
+  D=$(dirname $(realpath $0))
+  $D/../scripts/print-test.sh "php" "$TIME" "$SOLUTION"
+done
