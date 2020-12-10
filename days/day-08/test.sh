c437510156f9e95e3498f8dc57a5fb130261579d
Halvor <stektpotet@gmail.com>
2020-12-10 05:59:02 +0100

    solution for day08

--- days/day-08/test.sh
+++ days/day-08/test.sh
@@ -8,5 +8,6 @@ echo ""
 echo "--- Day 8: Handheld Halting ---"
 $D/../../languages/go.sh     $D/input.txt $D/output.txt $D/solutions/tholok97.go
 $D/../../languages/sml.sh    $D/input.txt $D/output.txt $D/solutions/day08.sml
+$D/../../languages/python.sh $D/input.txt $D/output.txt $D/solutions/day08.stektpotet.py
 $D/../../languages/deno.sh   $D/input.txt $D/output.txt $D/solutions/day08.ts
 echo ""
