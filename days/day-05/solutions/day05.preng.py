4159e5f17756615a360bad1fa77477e6892e782f
Jonas <jonasjso@pm.me>
2020-12-12 11:17:07 +0100

    Revert "Sync MO fork and Arxis main (#86)"
    This reverts commit 0652c1ab912ca490928c0edd62d6612fc071da5d.

--- days/day-05/solutions/day05.preng.py
+++ days/day-05/solutions/day05.preng.py
@@ -9,12 +9,17 @@ seats = [0 for i in range(8*128)]
 
 for line in get_stdin():
     pos = 0
-    delta = 8 * 64
+    dy = 64 * 8
     row = 0
-    for char in line:
-        if char in ("B", "R"):
-            pos += delta
-        delta = delta // 2
+    for ychar in line[:7]:
+        if ychar == "B":
+            pos += dy
+        dy = dy // 2
+    dx = 4
+    for xchar in line[7:]:
+        if xchar == "R":
+            pos += dx
+        dx = dx // 2
     seats[pos] = 1
     if pos > max:
         max = pos
