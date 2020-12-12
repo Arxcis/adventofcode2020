763f78f9c34bbf87fa5339bb55faf0b3b9810a85
Jonas Johan Solsvik <jonasjso@protonmail.com>
2020-12-12 21:32:10 +0100

    3x faster day 11 deno.ts

--- lib/raycast.ts
+++ lib/raycast.ts
@@ -16,28 +16,30 @@
 export function raycastEightDirections<Cell>(
   grid: Cell[][], row:number, col:number, rayLimit: number, FLOOR: Cell
 ) {
-  let NW: Cell|null = null, N: Cell|null = null, NE: Cell|null = null, W: Cell|null = null, E: Cell|null = null, SW: Cell|null = null, S: Cell|null = null, SE: Cell|null = null
-
-  // Ray cast in 8 directions
-  for (let ray = 1; ray <= rayLimit; ++ray) {
-    NW =  NW ?? (grid[row - ray]?.[col - ray] === FLOOR ? null : grid[row - ray]?.[col - ray] ?? null);
-    N  =  N  ?? (grid[row - ray]?.[col      ] === FLOOR ? null : grid[row - ray]?.[col      ] ?? null);
-    NE =  NE ?? (grid[row - ray]?.[col + ray] === FLOOR ? null : grid[row - ray]?.[col + ray] ?? null);
-    W  =  W  ?? (grid[row      ]?.[col - ray] === FLOOR ? null : grid[row      ]?.[col - ray] ?? null);
-    E  =  E  ?? (grid[row      ]?.[col + ray] === FLOOR ? null : grid[row      ]?.[col + ray] ?? null);
-    SW =  SW ?? (grid[row + ray]?.[col - ray] === FLOOR ? null : grid[row + ray]?.[col - ray] ?? null);
-    S  =  S  ?? (grid[row + ray]?.[col      ] === FLOOR ? null : grid[row + ray]?.[col      ] ?? null);
-    SE =  SE ?? (grid[row + ray]?.[col + ray] === FLOOR ? null : grid[row + ray]?.[col + ray] ?? null);
-  }
-
-  NW =  NW ?? FLOOR
-  N  =  N  ?? FLOOR
-  NE =  NE ?? FLOOR
-  W  =  W  ?? FLOOR
-  E  =  E  ?? FLOOR
-  SW =  SW ?? FLOOR
-  S  =  S  ?? FLOOR
-  SE =  SE ?? FLOOR
+  const NW = raycast(rayLimit, FLOOR, ray => grid[row - ray]?.[col - ray])
+  const N  = raycast(rayLimit, FLOOR, ray => grid[row - ray]?.[col      ])
+  const NE = raycast(rayLimit, FLOOR, ray => grid[row - ray]?.[col + ray])
+  const W  = raycast(rayLimit, FLOOR, ray => grid[row      ]?.[col - ray])
+  const E  = raycast(rayLimit, FLOOR, ray => grid[row      ]?.[col + ray])
+  const SW = raycast(rayLimit, FLOOR, ray => grid[row + ray]?.[col - ray])
+  const S  = raycast(rayLimit, FLOOR, ray => grid[row + ray]?.[col      ])
+  const SE = raycast(rayLimit, FLOOR, ray => grid[row + ray]?.[col + ray])
 
   return [NW, N, NE, W, E, SW, S, SE]
+}
+
+function raycast<Cell>(
+  rayLimit: number, FLOOR: Cell, getCell: (ray: number) => Cell|undefined
+): Cell {
+  for (let ray = 1; ray <= rayLimit; ++ray) {
+    const cell = getCell(ray);
+    if (cell === undefined) {
+      return FLOOR;
+    }
+    if (cell !== FLOOR) {
+      return cell;
+    }
+  }
+
+  return FLOOR;
 }
