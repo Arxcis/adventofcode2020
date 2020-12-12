f8f88b3b29e24204ce41aa37efc2ee2d366e23fe
Jonas Johan Solsvik <jonasjso@protonmail.com>
2020-12-13 00:23:38 +0100

    add README 11 and 12

--- days/day-02/test.sh
+++ days/day-02/test.sh
@@ -5,11 +5,12 @@ D=$(dirname $(realpath $0))
 
 echo ""
 echo "--- Day 2: Password Philosophy ---"
-$D/../../lang/go.sh     "$D/solutions/*.go"   "$D/io/*"
+$D/../../lang/go.sh     "$D/solutions/day02.stektpotet.go"   "$D/io/*"
 $D/../../lang/c.sh      "$D/solutions/*.c"    "$D/io/*"
 $D/../../lang/zig.sh    "$D/solutions/*.zig"  "$D/io/*"
 $D/../../lang/cpp.sh    "$D/solutions/*.cpp"  "$D/io/*"
 $D/../../lang/sml.sh    "$D/solutions/*.sml"  "$D/io/*"
+$D/../../lang/go.sh     "$D/solutions/day02.tholok97.go"   "$D/io/*"
 $D/../../lang/python.sh "$D/solutions/*.py"   "$D/io/*"
 $D/../../lang/deno.sh   "$D/solutions/*.ts"   "$D/io/*"
 echo ""
--- days/day-07/test.sh
+++ days/day-07/test.sh
@@ -8,8 +8,8 @@ echo ""
 echo "--- Day 7: Handy Haversacks ---"
 
 $D/../../lang/sml.sh    "$D/solutions/*.sml"  "$D/io/*"
-$D/../../lang/go.sh     "$D/solutions/*.go"   "$D/io/*"
 $D/../../lang/python.sh "$D/solutions/*.py"   "$D/io/*"
+$D/../../lang/go.sh     "$D/solutions/*.go"   "$D/io/*"
 $D/../../lang/node.sh   "$D/solutions/*.mjs"  "$D/io/*"
 $D/../../lang/deno.sh   "$D/solutions/*.ts"   "$D/io/*"
 
