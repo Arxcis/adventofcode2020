a1097dae64dbdbe5d854418d1f162f32caf06cab
Jonas Johan Solsvik <jonasjso@protonmail.com>
2020-12-13 20:20:02 +0100

    solve day10 in deno.ts

--- days/day-10/solutions/day10.ts
+++ days/day-10/solutions/day10.ts
@@ -12,31 +12,24 @@ let joltageDiffCount = { [1]: 0, [3]: 0 }
 /** --- part 1 --- */
 for (let i = 1; i < boundedJoltages.length; ++i) {
   const joltageDiff: 1 | 3 = boundedJoltages[i] - boundedJoltages[i - 1] as 1 | 3;
-
   joltageDiffCount[joltageDiff] = joltageDiffCount[joltageDiff] + 1
 }
+console.log(`${joltageDiffCount[1] * joltageDiffCount[3]}`)
 
-
+/** --- part 2 --- */
 let combinations = 1;
+for (let i = 1; i < boundedJoltages.length; ++i) {
 
-for (let i = 0; i < boundedJoltages.length;) {
-  const arr = boundedJoltages;
-  const it = boundedJoltages[i];
-
-  const first = (arr[i + 1] !== undefined && arr[i + 1] - it <= 3) ? 1 : 0;
-  const second = (arr[i + 2] !== undefined && arr[i + 2] - it <= 3) ? 1 : 0;
-  const third = (arr[i + 3] !== undefined && arr[i + 3] - it <= 3) ? 1 : 0;
-
-  const comb = Math.max(1, first + second + third)
-
-  combinations *= comb;
-
-  if (first && second && third) {
-    ++i; ++i;
-  } else {
-    ++i;
+  // Compute combinations per +1-group: (4,5,6,7,8) -> 1+(0+1+2+3+4) = 10 combinations
+  let plusOneGroupCombinations = 1
+  for (let j = 0; boundedJoltages[i] - boundedJoltages[i-1] === 1; ++j, ++i) {
+    plusOneGroupCombinations += j
   }
+
+  // Multiply +1-groups together, to get the total combinations:
+  //
+  // comb(1) * comb(4,5,6,7,8) * comb(11,12,13) -> (1)*(10)*(2) = 20
+  combinations *= plusOneGroupCombinations
 }
 
-console.log(joltageDiffCount[1] * joltageDiffCount[3])
-console.log(combinations)
+console.log(`${combinations}`)
