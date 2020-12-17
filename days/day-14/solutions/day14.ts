61bc180ffa8ae1756a02cc223daacc69c10ba199
Jonas Johan Solsvik <jonasjso@protonmail.com>
2020-12-17 22:44:44 +0100

    solve day14 in deno.ts

--- days/day-14/solutions/day14.ts
+++ days/day-14/solutions/day14.ts
@@ -1,41 +1,92 @@
 const input = (await Deno.readTextFile("/dev/stdin"))
-const maskGroups = input.matchAll(/mask = [X10]+\n(mem\[\d+\] = \d+\n)+/g)
-const memoryMap = new Map()
+const maskGroups = [...input.matchAll(/mask = [X10]+\n(mem\[\d+\] = \d+\n)+/g)]
 
-for (const [group] of [...maskGroups]) {
-  const maskString = [...(group.match(/[X10]{36}/)?.[0] ?? "")].reverse() as ("X"|"1"|"0")[]
+//
+// --- Part 1 ---
+//
+{
+  const memoryMap = new Map()
+  for (const [group] of [...maskGroups]) {
+    const maskString = [...(group.match(/[X10]{36}/)?.[0] ?? "")].reverse() as ("X"|"1"|"0")[]
 
-  let SET_ONE_MASK  = 0x00000000000000000000000000000000
-  let SET_ZERO_MASK = 0xffffffffffffffffffffffffffffffff
+    let SET_ONE_MASK  = 0x00000000000000000000000000000000n
+    let SET_ZERO_MASK = 0xffffffffffffffffffffffffffffffffn
 
-  console.log(maskString.join())
-  for (let i = 0; i < maskString.length; ++i) {
-    const char = maskString[i]
+    for (let i = 0; i < maskString.length; ++i) {
+      const char = maskString[i]
 
-    switch (char) {
-      case "1": { SET_ONE_MASK |= 1 << i } break;
-      case "0": { SET_ZERO_MASK &= (~(1 << i)) } break;
-      case "X": {} break;
+      switch (char) {
+        case "1": { SET_ONE_MASK |= 1n << BigInt(i) } break;
+        case "0": { SET_ZERO_MASK &= (~(1n << BigInt(i))) } break;
+        case "X": {} break;
+      }
     }
-    console.log(i, SET_ONE_MASK, SET_ZERO_MASK, 1 << i, (~(1 << i)))
+
+    const initLines = [...group
+        .matchAll(/mem\[(\d+)\] = (\d+)/g)]
+
+    const initValues = initLines
+      .map(([,index, value]) => ([parseInt(index), BigInt(value)])) as [number, bigint][]
+
+    for (let [index, value] of initValues) {
+      value |= SET_ONE_MASK
+      value &= SET_ZERO_MASK
+
+      memoryMap.set(index, value)
+    }
   }
-
-  break;
-
-  const initLines = [...group
-      .matchAll(/mem\[(\d+)\] = (\d+)/g)]
-
-  const initValues = initLines
-    .map(([,index, value]) => ([parseInt(index), parseInt(value)]))
-
-  // --- part1 ---
-  for (let [index, value] of initValues) {
-    value |= SET_ONE_MASK
-    value &= SET_ZERO_MASK
-
-    memoryMap.set(index, value)
-  }
+  const sum = [...memoryMap.values()].reduce((acc,it) => acc+it, 0n)
+  console.log(`${Number(sum)}`)
 }
 
-const sum = [...memoryMap.values()].reduce((acc,it) => acc+it, 0)
-console.log(sum)
+//
+// Part 2
+//
+{
+  const memoryMap = new Map()
+  for (const [group] of [...maskGroups]) {
+    const maskString = [...(group.match(/[X10]{36}/)?.[0] ?? "")] as ("X"|"1"|"0")[]
+
+    const memoryInitLines = [...group
+      .matchAll(/mem\[(\d+)\] = (\d+)/g)]
+    const memoryInits = memoryInitLines
+      .map(([,address, value]) => ([BigInt(address), BigInt(value)])) as [bigint, bigint][]
+
+
+    for (let [address, value] of memoryInits) {
+      let addressCombinations = [address]
+
+      // Enumerate addresses by X's (free floating bits either 0 or 1)
+      for (let i = maskString.length-1; i >= 0; --i) {
+        const char = maskString[maskString.length-1-i]
+        switch (char) {
+          case "X": {
+            addressCombinations = addressCombinations
+              .reduce((acc, it) => [
+                ...acc,
+                it | 1n << BigInt(i),
+                it & ~(1n << BigInt(i))
+              ], [] as bigint[])
+          } break;
+        }
+      }
+
+      // OR 1's
+      for (let i = maskString.length-1; i >= 0; --i) {
+        const char = maskString[maskString.length-1-i]
+        switch (char) {
+          case "1": {
+            addressCombinations = addressCombinations
+              .map(it => it | 1n << BigInt(i))
+          } break;
+        }
+      }
+
+      for (const address of addressCombinations) {
+        memoryMap.set(address, value)
+      }
+    }
+  }
+  const sum = [...memoryMap.values()].reduce((acc,it) => acc+it, 0n)
+  console.log(`${Number(sum)}`)
+}
