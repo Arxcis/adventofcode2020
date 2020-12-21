c5c06654995eed5ebd2d3a178ef4115a2d565206
Jonas Johan Solsvik <jonasjso@protonmail.com>
2020-12-21 22:51:46 +0100

    solve day16 in deno.ts

--- days/day-16/solutions/day16.ts
+++ days/day-16/solutions/day16.ts
@@ -5,13 +5,13 @@ const ticketMatches = [...input.matchAll(/(\d+(,\d+)+)\n/g)]
 type Rule = {
   name: string;
   ranges: [number, number][]
-  position: number;
+  possiblePositions: number[];
 }
 
 const rules = ruleMatches.map(([,name, firstRangeFrom, firstRangeTo, lastRangeFrom, lastRangeTo]) => ({
   name,
   ranges: [[parseInt(firstRangeFrom), parseInt(firstRangeTo)], [parseInt(lastRangeFrom), parseInt(lastRangeTo)]],
-  position: -1,
+  possiblePositions: [],
 })) as Rule[];
 
 type Ticket = {
@@ -22,7 +22,6 @@ const [myTicket, ...nearbyTickets] = ticketMatches.map(([commaSeparatedNumbers])
   numbers: commaSeparatedNumbers.split(",").map((num: string) => parseInt(num))
 })) as Ticket[];
 
-
 //
 // Part 1
 //
@@ -30,8 +29,8 @@ const [myTicket, ...nearbyTickets] = ticketMatches.map(([commaSeparatedNumbers])
   let ticketScanningErrorRate = 0
 
   for (const ticket of nearbyTickets) {
-
     for (const num of ticket.numbers) {
+
       const invalid = rules
         .every(rule => rule.ranges
           .every(([from, to]) => num < from || to < num))
@@ -49,45 +48,54 @@ const [myTicket, ...nearbyTickets] = ticketMatches.map(([commaSeparatedNumbers])
 // Part 2
 //
 {
-  let validTickets = nearbyTickets
+  const validTickets = nearbyTickets
     .filter(ticket => ticket.numbers
       .every(num => rules
         .some(rule => rule.ranges
           .some(([from, to]) => from <= num && num <= to))))
 
+  const allPositions = validTickets[0].numbers
+    .map((_,i) => i)
 
-  function recursivelyFindRuleCombinationThatWorks(rules: Rule[], numbers: number[])  {
-    if (rules.length !== numbers.length) {
-      console.error("rules.length !== numbers.length")
-      return null;
-    }
-
-    if (numbers.length === 1) {
-      const [rule] = rules;
-      rule.position = numbers.length - 1
-      return rules;
-    }
-
-    if (numbers.length === 2) {
-      const [rule, otherRule] = rules
-      const [number, otherNumber] = numbers
-    }
-
-    const [number, ...restNumbers] = numbers
-
-
+  // 1. Find all possible positions per rule
+  for (const rule of rules) {
+    rule.possiblePositions = allPositions
+      .filter(i => validTickets
+        /** @assumption Every ticket has the same amount of numbers, which is why it is safe to use [i] here */
+        .map(ticket => ticket.numbers[i])
+        .every(num => rule.ranges
+          .some(([from, to]) => from <= num && num <= to)))
   }
 
-console.log(rules, myTicket)
+  // 2. Reduce possibilities to exactly 1 possibility per rule, by elimination method
+  let allRules = JSON.parse(JSON.stringify(rules)) as Rule[]
+  const rulesWithExactlyOnePossiblePosition = [] as Rule[];
+  for (
+    let foundIndex = allRules.findIndex(rule => rule.possiblePositions.length === 1);
+    foundIndex !== -1;
+    foundIndex = allRules.findIndex(rule => rule.possiblePositions.length === 1)
+  ) {
+    // 3. Copy and push
+    const ruleWithExactlyOnePossiblePosition = JSON.parse(JSON.stringify(allRules[foundIndex])) as Rule;
+    rulesWithExactlyOnePossiblePosition.push(ruleWithExactlyOnePossiblePosition)
 
-  // const rulesThatStartWithDeparture = rules
-  //   .filter(rule => rule.position !== -1)
-  //   .filter(rule => rule.name.startsWith("departure"))
+    // 4. Go through all rules, and remove the found possible position
+    allRules = allRules
+      .map(otherRule => ({
+        ...otherRule,
+        possiblePositions: otherRule.possiblePositions
+          .filter(possiblePosition => possiblePosition !== ruleWithExactlyOnePossiblePosition.possiblePositions[0])
+      }))
+  }
 
-  // console.log(rulesThatStartWithDeparture)
-  // console.log(myTicket)
-  // const answer = rulesThatStartWithDeparture
-  //   .reduce((acc, rule) => acc * myTicket.numbers[rule.position], 1)
+  const rulesThatStartWithDeparture = rulesWithExactlyOnePossiblePosition
+    .filter(rule => rule.name.startsWith("departure"))
 
-  // console.log(`${answer}`)
+  const departueIndicies = rulesThatStartWithDeparture
+    .map(rule => rule.possiblePositions[0])
+
+  const answerPart2 = departueIndicies
+    .reduce((acc, i) => acc * myTicket.numbers[i], 1)
+
+  console.log(`${answerPart2}`)
 }
