963dfb6001461381532897719e69bf84e77d2b15
Jonas Johan Solsvik <jonasjso@protonmail.com>
2020-12-22 01:03:00 +0100

    improve performance by using plain old numbers, instead of object

--- days/day-15/lib/whatIsTheNthSpokenNumber.ts
+++ days/day-15/lib/whatIsTheNthSpokenNumber.ts
@@ -9,55 +9,45 @@ export function whatIsTheNthSpokenNumber(initialNumbers: number[], N: number): n
   //
   // Inital round where everyone speaks the initial numbers in sequential order
   //
-  const spokenNumbers = initialNumbers.map((num, i) => ({
-    value: num,
-    lastSpokenTurn: i+1,
-  })) as SpokenNumberType[];
-
-  const spokenMap = new Array(N)
-  for (const num of spokenNumbers) {
-    spokenMap[num.value] = num;
+  const turnBeforeThatMap = new Array(N)
+  const lastTurnMap = new Array(N)
+  for (let i = 0; i < initialNumbers.length; ++i) {
+    lastTurnMap[initialNumbers[i]] = i+1;
   }
+
   //
   // Actual game starts here, where the next number is calculated based on what was previously spoken.
   //
 
   // if initial numbers are [1,2,3] -> should be 3th turn now with lastspoken number 3
-  let turn = spokenNumbers.length;
-  let lastSpokenNumber = spokenNumbers[spokenNumbers.length-1]
-
-  let zeroNumber = spokenMap[0] ?? { value: 0, lastSpokenTurn: 0 }
+  let turn = initialNumbers.length;
+  let lastSpokenNumber = initialNumbers[initialNumbers.length-1]
 
   while (turn < N) {
     ++turn;
 
     // If the last number is a NEW number which has not been spoken before, speak zero!
-    if (!lastSpokenNumber.spokenTurnBeforeThat) {
-      zeroNumber.spokenTurnBeforeThat = zeroNumber.lastSpokenTurn
-      zeroNumber.lastSpokenTurn = turn
-      lastSpokenNumber = zeroNumber
+    if (!turnBeforeThatMap[lastSpokenNumber]) {
+      turnBeforeThatMap[0] = lastTurnMap[0]
+      lastTurnMap[0] = turn
+      lastSpokenNumber = 0
     } else {
       // ...else the diff is the next spoken number
-      const diff = lastSpokenNumber.lastSpokenTurn - lastSpokenNumber.spokenTurnBeforeThat
+      const nextSpokenNumber = lastTurnMap[lastSpokenNumber] - turnBeforeThatMap[lastSpokenNumber]
 
       // if the next spoken number been spoken before...
-      const nextSpokenNumber = spokenMap[diff]
-      if (nextSpokenNumber) {
-        nextSpokenNumber.spokenTurnBeforeThat = nextSpokenNumber.lastSpokenTurn;
-        nextSpokenNumber.lastSpokenTurn = turn;
+      const exist = lastTurnMap[nextSpokenNumber]
+      if (exist) {
+        turnBeforeThatMap[nextSpokenNumber] = exist;
+        lastTurnMap[nextSpokenNumber] = turn;
         lastSpokenNumber = nextSpokenNumber
       } else {
         // ...else add the next spoken number to the map of spoken numbers
-        const nextSpokenNumber = {
-          value: diff,
-          lastSpokenTurn: turn,
-        } as SpokenNumberType
-
-        spokenMap[diff] = nextSpokenNumber
+        lastTurnMap[nextSpokenNumber] = turn
         lastSpokenNumber = nextSpokenNumber
       }
     }
   }
 
-  return lastSpokenNumber.value;
+  return lastSpokenNumber;
 }
