e98afb0e5c1d775165a1fa2b9eb5d5c9f54e24cd
Jonas Johan Solsvik <jonasjso@protonmail.com>
2020-12-23 11:59:09 +0100

    update README.md stats

--- days/day-22/test.sh
+++ days/day-22/test.sh
@@ -3,8 +3,10 @@ set -euo pipefail
 
 D=$(dirname $(realpath $0))
 
+echo ""
 echo "--- Day 22: --- Crab Combat ---"
 #$D/../../lang/go.sh     "$D/solutions/*.go"  "$D/io/*"
 #$D/../../lang/sml.sh    "$D/solutions/*.sml" "$D/io/*"
 $D/../../lang/python.sh "$D/solutions/*.py"  "$D/io/*"
 #$D/../../lang/deno.sh   "$D/solutions/*.ts"  "$D/io/*"
+echo ""
