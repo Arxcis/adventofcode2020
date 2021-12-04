cdff3abd09da8f7a7ccd2428df062cc190605193
Jonas Johan Solsvik <jonasjso@protonmail.com>
2021-12-04 12:36:51 +0100

    prettier workflow job names

--- scripts/make-workflows.sh
+++ scripts/make-workflows.sh
@@ -43,7 +43,7 @@ WORKFLOW
   for DAY in {day-01,day-02,day-03,day-04,day-05,day-06,day-07,day-08,day-09,day-10,day-11,day-12,day-13,day-14,day-15,day-16,day-17,day-18,day-19,day-20,day-21,day-22,day-23,day-24,day-25}
   do
     cat >> "./.github/workflows/$YEAR.yaml" << WORKFLOW
- test-${YEAR}-${DAY}:
+  ${DAY}:
     runs-on: ubuntu-latest
     container:
       image: ${DOCKER_TAG}
