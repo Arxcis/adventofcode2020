e015d17c5df31c3aba4e89c5a5a6ec2ee200cf37
Jonas <jonasjso@pm.me>
2021-12-04 19:53:24 +0100

    Add 2021/ (#122)
    * update make-folders.sh script
    
    * add 2021/

---
+++ 2021/day-04/test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+D=$(dirname $(realpath $0))
+
+echo "--- 2021/day-04: --- ????? --- ❌"
+#$D/../../lang/go.sh     "$D/solutions/*.go"  "$D/io/*"
+#$D/../../lang/sml.sh    "$D/solutions/*.sml" "$D/io/*"
+#$D/../../lang/python.sh "$D/solutions/*.py"  "$D/io/*"
+#$D/../../lang/deno.sh   "$D/solutions/*.ts"  "$D/io/*"
+
+exit 1337;
+
