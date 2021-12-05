533719e5a85d71bf6c151e1a1defadbd0c93222e
jonas johan solsvik <jonasjso@protonmail.com>
2021-12-05 20:44:35 +0100

    update workflows dockerimage

--- lang/print/success.sh
+++ lang/print/success.sh
@@ -6,4 +6,4 @@ TIME="$2ms"
 FILE="$3"
 
 # %-23s is to accomodate for the longest known solution name: "one-liner.stektpotet.bash"
-printf "cat INPUT | %-8s %-23s %-7s    ✅\n" "$CMD" "$(basename -- $FILE)" "$TIME"
+printf "cat \$INPUT | %-8s %-23s %-7s    ✅\n" "$CMD" "$(basename -- $FILE)" "$TIME"
