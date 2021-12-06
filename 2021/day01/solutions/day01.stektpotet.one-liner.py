089d4c412726cfd7f0971ea422038b6e3b69faf1
Halvor <stektpotet@gmail.com>
2021-12-06 14:45:00 +0100

    Naming cleanup and locally tested sys.stdin solution

--- 2021/day01/solutions/day01.stektpotet.one-liner.py
+++
@@ -1,1 +0,0 @@
-import sys; print(*(sum(s) for d in [[int(l) for l in sys.stdin.read().split('\n')]] for s in zip(*((d[i] < q, sum(d[i:i+3]) < sum(d[i+1:i+4])) for i, q in enumerate(d[1:])))), sep='\n')
---
+++ 2021/day01/solutions/stektpotet_one-liner.py
@@ -0,0 +1,1 @@
+import sys; print(*(sum(s) for d in [[int(l) for l in sys.stdin.read().split('\n')[:-1]]] for s in zip(*((d[i] < q, sum(d[i:i+3]) < sum(d[i+1:i+4])) for i, q in enumerate(d[1:])))), sep='\n')
