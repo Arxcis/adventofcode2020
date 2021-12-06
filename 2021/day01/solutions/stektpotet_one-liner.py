cc2ac4793132d40dac3fc893b60faf98ae6046ea
Halvor <stektpotet@gmail.com>
2021-12-06 15:40:01 +0100

    Patching enumeration circularity

--- 2021/day01/solutions/stektpotet_one-liner.py
+++ 2021/day01/solutions/stektpotet_one-liner.py
@@ -1,1 +1,1 @@
-import sys; print(*(sum(s) for d in [[int(l) for l in sys.stdin.read().split('\n')[:-1]]] for s in zip(*((d[i] < q, sum(d[i:i+3]) < sum(d[i+1:i+4])) for i, q in enumerate(d[1:])))), sep='\n')
+import fileinput; print(*(sum(s) for d in [[int(l) for l in fileinput.input()]] for s in [(d[i] < q for i, q in enumerate(d[1:])), (sum(d[i:i+3]) < sum(d[i+1:i+4]) for i, _ in enumerate(d[3:]))]), sep='\n')
