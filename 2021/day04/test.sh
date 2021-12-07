70922569a3ee5341f9dfc92244389de85683f571
Halvor <stektpotet@gmail.com>
2021-12-07 02:48:35 +0100

    Solve day04 patch

--- 2021/day04/test.sh
+++ 2021/day04/test.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 D=$(dirname $(realpath $0))
 
 echo "--- 2021/day04: Giant Squid ---"
-$D/../../lang/python.sh "$D/solutions/stektpote.py"  "$D/io/*"
+$D/../../lang/python.sh "$D/solutions/stektpotet.py"  "$D/io/*"
 #$D/../../lang/go.sh     "$D/solutions/*.go"  "$D/io/*"
 #$D/../../lang/sml.sh    "$D/solutions/*.sml" "$D/io/*"
 #$D/../../lang/deno.sh   "$D/solutions/*.ts"  "$D/io/*"
