32f202582ccf798c3e92eaaefca9106d258226d7
jonas johan solsvik <jonasjso@protonmail.com>
2021-12-10 23:36:07 +0100

    add rust to test.sh for day01

--- 2021/day01/test.sh
+++ 2021/day01/test.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 D=$(dirname $(realpath $0))
 
 echo "--- 2021/day01: Sonar Sweep ---"
+$D/../../lang/zig.sh     $D/solutions/avokado.zig  "$D/io/*"
+$D/../../lang/rust.sh    $D/solutions/arxcis.rs  "$D/io/*"
 $D/../../lang/python.sh  $D/solutions/arxcis.py  "$D/io/*"
 $D/../../lang/python.sh  $D/solutions/stektpotet.py  "$D/io/*"
 $D/../../lang/python.sh  $D/solutions/stektpotet_one-liner.py  "$D/io/*"
-$D/../../lang/zig.sh  $D/solutions/avokado.zig  "$D/io/*"
