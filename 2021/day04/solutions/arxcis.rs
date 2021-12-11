c825e9c9058d197b40c0163cfd41d5aee715469e
jonas johan solsvik <jonasjso@protonmail.com>
2021-12-12 00:19:19 +0100

    WIP day04 part 1

--- 2021/day04/solutions/arxcis.rs
+++ 2021/day04/solutions/arxcis.rs
@@ -1,72 +1,99 @@
 use std::{io, cmp, io::prelude::*};
+use std::collections::HashSet;
+use std::iter::FromIterator;
 
 fn main() -> io::Result<()> {
     let mut input = Vec::<String>::new();
     for line in io::stdin().lock().lines() {
         input.push(line?);
     }
-  
+    //
+    // Part 0 - Parse bingo boards and numbers
+    //
     let numbers: Vec<u8> = input[0].split(",").map(|val| val.parse().unwrap()).collect();
-    
-    let mut min_board = Vec::<Vec::<u8>>::new();
-    let mut min_board_attempts = 255;
+    let mut boards = Vec::<Vec::<Vec::<u8>>>::new();
 
     for i in (2..input.len()).step_by(6) {
-        
-        println!("{}", input[i]);
-        
-        for line in input[i+0].trim().split(" ") {
-            println!("{}", line);
-        }
 
-        let brow: Vec<u8> = input[i+0].trim().split(" ").map(|val|val.parse().unwrap()).collect();
+        let _b: Vec<u8> = input[i+0].trim().replace("  ", " ").split(" ").map(|val|val.parse().unwrap()).collect();
+        let _i: Vec<u8> = input[i+1].trim().replace("  ", " ").split(" ").map(|val|val.parse().unwrap()).collect();
+        let _n: Vec<u8> = input[i+2].trim().replace("  ", " ").split(" ").map(|val|val.parse().unwrap()).collect();
+        let _g: Vec<u8> = input[i+3].trim().replace("  ", " ").split(" ").map(|val|val.parse().unwrap()).collect();
+        let _o: Vec<u8> = input[i+4].trim().replace("  ", " ").split(" ").map(|val|val.parse().unwrap()).collect();
 
+        let b: Vec<u8> = vec![_b[0],_i[1],_n[2],_g[3],_o[4]];
+        let i: Vec<u8> = vec![_b[0],_i[1],_n[2],_g[3],_o[4]];
+        let n: Vec<u8> = vec![_b[0],_i[1],_n[2],_g[3],_o[4]];
+        let g: Vec<u8> = vec![_b[0],_i[1],_n[2],_g[3],_o[4]];
+        let o: Vec<u8> = vec![_b[0],_i[1],_n[2],_g[3],_o[4]];
 
-        let irow: Vec<u8> = input[i+1].trim().split(" ").map(|val|val.parse().unwrap()).collect();
-        let nrow: Vec<u8> = input[i+2].trim().split(" ").map(|val|val.parse().unwrap()).collect();
-        let grow: Vec<u8> = input[i+3].trim().split(" ").map(|val|val.parse().unwrap()).collect();
-        let orow: Vec<u8> = input[i+4].trim().split(" ").map(|val|val.parse().unwrap()).collect();
+        let board = vec![_b,_i,_n,_g,_o,b,i,n,g,o];
 
-        let bcol: Vec<u8> = vec![brow[0],irow[1],nrow[2],grow[3],orow[4]];
-        let icol: Vec<u8> = vec![brow[0],irow[1],nrow[2],grow[3],orow[4]];
-        let ncol: Vec<u8> = vec![brow[0],irow[1],nrow[2],grow[3],orow[4]];
-        let gcol: Vec<u8> = vec![brow[0],irow[1],nrow[2],grow[3],orow[4]];
-        let ocol: Vec<u8> = vec![brow[0],irow[1],nrow[2],grow[3],orow[4]];
-
-        let board = vec![brow,irow,nrow,grow,orow,bcol,icol,ncol,gcol,ocol];
-    
-        let mut min_line_attempts = 255;
-
-        for line in &board[..] {
-            let mut max_cell_attempts = 0;
-
-            for cell in &line[..] {
-                let mut attempts = 0;
-
-                for num in &numbers[..] {
-                    if num != cell {
-                        attempts += 1;
-                        continue; 
-                    } else {
-                        break;   
-                    }
-                }
-                max_cell_attempts = cmp::max(attempts, max_cell_attempts);
-            }
-            min_line_attempts = cmp::min(
-                max_cell_attempts,
-                min_line_attempts, 
-            );
-        }
-        
-        if min_line_attempts < min_board_attempts {
-            println!("{}", min_line_attempts);
-            min_board_attempts = min_line_attempts;
-            min_board = board;
-        }
+        boards.push(board);
     }
-    
-    println!("{:?}", min_board);
+    //
+    // Part 1
+    //
+    {
+        let mut winning_board = Vec::<Vec::<u8>>::new();
+        let mut min_board_attempts = 255;
+
+        for board in boards {
+            let mut min_line_attempts = 255;
+
+            for line in &board[..] {
+                let mut max_cell_attempts = 0;
+
+                for cell in &line[..] {
+                    let mut attempts = 0;
+
+                    for num in &numbers[..] {
+                        attempts += 1;
+                        if num == cell {
+                            break;
+                        }
+                    }
+                    max_cell_attempts = cmp::max(attempts, max_cell_attempts);
+                }
+                min_line_attempts = cmp::min(max_cell_attempts,min_line_attempts);
+            }
+            min_board_attempts = cmp::min(min_line_attempts, min_board_attempts);
+
+            if min_line_attempts == min_board_attempts {
+                winning_board = board;
+            }
+        }
+        let winning_number = u64::from(numbers[min_board_attempts-1]);
+
+        let winning_board: HashSet<u64> = HashSet::from_iter(
+            winning_board.iter()
+                .flatten()
+                .map(|num| u64::from(*num))
+                .collect::<Vec<u64>>()
+                .iter()
+                .cloned()
+        );
+
+        let said_numbers: Vec<u64> = numbers.iter()
+            .take(min_board_attempts)
+            .map(|num| u64::from(*num))
+            .collect();
+
+        let marked_numbers: Vec<&u64> = winning_board.iter()
+            .filter(|number| said_numbers.contains(number))
+            .collect();
+
+        let unmarked_numbers: Vec<u64> = winning_board.iter()
+            .filter(|number| !marked_numbers.contains(number))
+            .map(|num| u64::from(*num))
+            .collect();
+
+        println!("{:?}\n{:?}\n{:?}\n{:?}", winning_board, said_numbers, marked_numbers, unmarked_numbers);
+
+        let unmarked_sum: u64 = unmarked_numbers.iter().sum();
+        println!("{}", winning_number * unmarked_sum);
+    }
+
     Ok(())
 }
 
