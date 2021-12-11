32ac6c05f62d22bf23e69f409382c7ee5c7130a7
Aksel Hjerpbakk <akselhallajs@gmail.com>
2021-12-11 11:19:50 +0100

    2021 Day02 in Zig by Avokadoen (#143)

--- 2021/day02/test.sh
+++ 2021/day02/test.sh
@@ -5,3 +5,4 @@ D=$(dirname $(realpath $0))
 
 echo "* day02: Dive! -----------------------------------------------"
 $D/../../lang/rust.sh   "$D/solutions/arxcis.rs"  "$D/io/*"
+$D/../../lang/zig.sh   "$D/solutions/avokado.zig"  "$D/io/*"
