d9f48d89c492b2f28dfaecdfe016310e0fd2bd4d
jonas johan solsvik <jonasjso@protonmail.com>
2021-12-15 23:31:13 +0100

    keep incomplete lines

--- 2021/day10/solutions/arxcis.rs
+++ 2021/day10/solutions/arxcis.rs
@@ -13,17 +13,19 @@ fn main() -> io::Result<()> {
     //
     // Part 1
     //
+    let mut incomplete_lines = Vec::<String>::new();
     {
         let mut stack = Vec::<char>::new();
         let mut points = 0;
 
         for line in lines.iter() {
+            let mut syntax_error = '?';
             for current in line.chars() {
+
                 match current {
                     '['|'('|'{'|'<' => stack.push(current),
                     ']'|')'|'}'|'>' => {
                         let prev = stack.pop().unwrap();
-                        let mut syntax_error = '?';
                         match prev {
                             '(' if current != ')' => syntax_error = current,
                             '[' if current != ']' => syntax_error = current,
@@ -31,20 +33,28 @@ fn main() -> io::Result<()> {
                             '<' if current != '>' => syntax_error = current,
                             _ => (),
                         }
-                        match syntax_error {
-                            ')' => points += 3,
-                            ']' => points += 57,
-                            '}' => points += 1197,
-                            '>' => points += 25137,
-                            _ => (),
-                        }
                     },
                     _ => panic!("ERROR uknown char: {}", current)
                 }
+                match syntax_error {
+                    ')' => {points += 3; break;},
+                    ']' => {points += 57; break;},
+                    '}' => {points += 1197; break;},
+                    '>' => {points += 25137; break;},
+                    _ => (),
+                }
+            }
+            if syntax_error == '?' {
+                incomplete_lines.push(line.to_string());
             }
         }
         println!("{}", points);
     }
+    //
+    // Part 2
+    //
+    {
 
+    }
     Ok(())
 }
