421cd60851ab70c3e347ccd2fca21d3753d7f780
jonas johan solsvik <jonasjso@protonmail.com>
2021-12-17 19:09:57 +0100

    jellyfish -> octopuses

--- 2021/day11/solutions/arxcis.rs
+++ 2021/day11/solutions/arxcis.rs
@@ -7,45 +7,45 @@ fn main() -> io::Result<()> {
     //
     // Parse
     //
-    let mut jellyfish = Vec::<Vec<u64>>::new();
+    let mut octopuses = Vec::<Vec<u64>>::new();
     let mut i = 0;
 
     for line in io::stdin().lock().lines() {
         let line = line?;
-        
-        let mut jellyfishline = Vec::<u64>::new();
-        jellyfishline.push(PADDING);
+
+        let mut octopusline = Vec::<u64>::new();
+        octopusline.push(PADDING);
         for ch in line.chars() {
-            jellyfishline.push(u64::from(ch)-48); 
+            octopusline.push(u64::from(ch)-48);
         }
-        jellyfishline.push(PADDING);
+        octopusline.push(PADDING);
 
         if i == 0 {
             let mut pad = Vec::<u64>::new();
-            for _ in jellyfishline.iter() {
+            for _ in octopusline.iter() {
                 pad.push(PADDING);
             }
-            jellyfish.push(pad);
+            octopuses.push(pad);
             i += 1;
         }
-        jellyfish.push(jellyfishline);
+        octopuses.push(octopusline);
     }
     let mut pad = Vec::<u64>::new();
-    for _ in jellyfish[0].iter() {
+    for _ in octopuses[0].iter() {
         pad.push(PADDING);
     }
-    jellyfish.push(pad);
+    octopuses.push(pad);
     //
     // Simulate step
     //
-    let simulate = |jellyfish: &mut Vec::<Vec<u64>>| -> u64 {
+    let simulate = |octopuses: &mut Vec::<Vec<u64>>| -> u64 {
         // First, the energy level of each octopus increases by 1.
-        for y in 1..jellyfish.len() - 1 {
-            for x in 1..jellyfish[y].len() - 1 {
-                jellyfish[y][x] += 1;
+        for y in 1..octopuses.len() - 1 {
+            for x in 1..octopuses[y].len() - 1 {
+                octopuses[y][x] += 1;
             }
         }
-       
+
         // Then, any octopus with an energy level greater than 9 flashes. This increases the energy level of all adjacent octopuses by 1, including octopuses that are diagonally adjacent. If this causes an octopus to have an energy level greater than 9, it also flashes. This process continues as long as new octopuses keep having their energy level increased beyond 9. (An octopus can only flash at most once per step.)
         let mut flashed = HashSet::<(usize,usize)>::new();
         let mut total_flashes: u64 = 0;
@@ -53,25 +53,25 @@ fn main() -> io::Result<()> {
         loop {
             let mut flash_count = 0;
 
-            for y in 1..jellyfish.len() - 1 {
-                for x in 1..jellyfish[y].len() - 1 {
+            for y in 1..octopuses.len() - 1 {
+                for x in 1..octopuses[y].len() - 1 {
 
                     if flashed.contains(&(x,y)) {
                         continue;
                     }
 
-                    if jellyfish[y][x] > 9 {
+                    if octopuses[y][x] > 9 {
                         flash_count += 1;
                         flashed.insert((x,y));
 
-                        jellyfish[y][x-1] += 1;
-                        jellyfish[y][x+1] += 1;
-                        jellyfish[y-1][x-1] += 1;
-                        jellyfish[y-1][x] += 1;
-                        jellyfish[y-1][x+1] += 1;
-                        jellyfish[y+1][x-1] += 1;
-                        jellyfish[y+1][x] += 1;
-                        jellyfish[y+1][x+1] += 1;
+                        octopuses[y][x-1] += 1;
+                        octopuses[y][x+1] += 1;
+                        octopuses[y-1][x-1] += 1;
+                        octopuses[y-1][x] += 1;
+                        octopuses[y-1][x+1] += 1;
+                        octopuses[y+1][x-1] += 1;
+                        octopuses[y+1][x] += 1;
+                        octopuses[y+1][x+1] += 1;
                     }
                 }
             }
@@ -81,10 +81,10 @@ fn main() -> io::Result<()> {
             }
         }
         // Finally, any octopus that flashed during this step has its energy level set to 0, as it used all of its energy to flash.
-        for y in 1..jellyfish.len() - 1 {
-            for x in 1..jellyfish[y].len() - 1 {
-                if jellyfish[y][x] > 9 {
-                    jellyfish[y][x] = 0;
+        for y in 1..octopuses.len() - 1 {
+            for x in 1..octopuses[y].len() - 1 {
+                if octopuses[y][x] > 9 {
+                    octopuses[y][x] = 0;
                 }
             }
         }
@@ -94,10 +94,10 @@ fn main() -> io::Result<()> {
     // Part 1
     //
     let mut n = 0;
-    {    
+    {
         let mut total_flashes: u64 = 0;
         for _ in 0..100 {
-            total_flashes += simulate(&mut jellyfish);
+            total_flashes += simulate(&mut octopuses);
             n += 1;
         }
 
@@ -106,20 +106,21 @@ fn main() -> io::Result<()> {
     //
     // Part 2
     //
-    { 
+    {
+        // What is the first step during which all octopuses flash?
         loop {
-            simulate(&mut jellyfish);
+            simulate(&mut octopuses);
             n += 1;
-            
-            let mut every_jellyfish_in_sync = true;
-            for y in 1..jellyfish.len() - 1 {
-                for x in 1..jellyfish[y].len() - 1 {
-                    if jellyfish[y][x] != 0 {
-                         every_jellyfish_in_sync = false;
+
+            let mut every_octopuses_in_sync = true;
+            for y in 1..octopuses.len() - 1 {
+                for x in 1..octopuses[y].len() - 1 {
+                    if octopuses[y][x] != 0 {
+                         every_octopuses_in_sync = false;
                     }
                 }
             }
-            if every_jellyfish_in_sync == true {
+            if every_octopuses_in_sync == true {
                 break;
             }
         }
