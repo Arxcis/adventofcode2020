3a59cf77afe959ee64b48d1fe7a250f291b5f5d6
Halvor <stektpotet@gmail.com>
2021-12-18 22:18:09 +0100

    Solve day08 in python (#156)

--- 2021/day08/test.sh
+++ 2021/day08/test.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 D=$(dirname $(realpath $0))
 
 echo "# Day 8: Seven Segment Search ---------------------------------"
+$D/../../lang/python.sh "$D/solutions/stektpotet.py"  "$D/io/*"
 $D/../../lang/rust.sh   "$D/solutions/arxcis.rs"  "$D/io/*"
-
