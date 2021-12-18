81c02ef61af35ff9ff4209b5b40a2246051a8988
Jonas Johan Solsvik <jonasjso@protonmail.com>
2021-12-18 19:58:24 +0100

    remove deadcode

--- 2021/day14/solutions/arxcis.rs
+++ 2021/day14/solutions/arxcis.rs
@@ -39,40 +39,6 @@ fn main() -> io::Result<()> {
     //
     // Part 1 - Apply 10 steps of pair insertion to the polymer template and find the most and least common elements in the result.
     //
-    // {
-    //     let mut polymer = template.clone();
-
-    //     let now = std::time::Instant::now();
-    //     for _ in 0..25 {
-    //         polymer = extend_polymer_naive(&polymer, &rules);
-    //     }
-    //     println!("total elapsed: {}ms", now.elapsed().as_millis());
-
-    //     for byte in polymer {
-    //         counts[byte as usize] += 1;
-    //     }
-    //     counts.sort_by(|a,b| b.cmp(a));
-    //     let counts = counts.iter().filter(|&a| *a != 0).map(|a| u64::from(*a)).collect::<Vec<u64>>();
-    //     let max = counts[0];
-    //     let min = counts[counts.len() - 1];
-    //     println!("{:?}", max-min);
-    // }
-    // @optimization
-  //  {
-     //   for i in 0..template.len()-1 {
-       //     let current = template[i] as usize;
-         //   let next = template[i+1] as usize;
-        //    counts[current] += 1;
-        //    extend_polymer_recursive(&rules, current, next, 0, 25, &mut counts);
-     //   }
- //       counts[template[template.len()-1] as usize] += 1;
-//
-   //     counts.sort_by(|a,b| b.cmp(a));
-       // let counts = counts.into_iter().filter(|&a| a != 0).collect::<Vec<u64>>();
-     //   let max = counts[0];
-      //  let min = counts[counts.len() - 1];
-     //   println!("{:?}", max-min);
-   // }
     {
         let mut pairs: Vec<u64> = vec![0; 1 << 13]; // 8kB * 8 = 64kB
         // Init pairs
@@ -126,44 +92,4 @@ fn main() -> io::Result<()> {
         println!("{:?}", max-min);       
     }
     Ok(())
-}
-
-
-fn _extend_polymer_recursive(rules: &Vec<u8>, current: usize, next: usize, i: usize, n: usize, counts: &mut Vec<u64>)  {
-    let pair: usize = current << 8 | next;
-    let insert = rules[pair] as usize;
-
-    counts[insert] += 1;
-
-    if i + 1 < n {
-        _extend_polymer_recursive(rules, current, insert, i + 1, n, counts);
-        _extend_polymer_recursive(rules, insert, next, i + 1, n, counts);
-    }
-}
-
-
-fn _extend_polymer_naive(polymer: &Vec<u8>, rules: &Vec<u8>) -> Vec<u8> {
-
-    let mut next_polymer: Vec<u8> = Vec::with_capacity(polymer.len() * 2);
-
-    for i in 0..polymer.len()-1 {
-        // 27%
-        let current = polymer[i];
-        // 25%
-        let next = polymer[i+1];
-        // 15%
-        let key: usize = (current as usize) << 8 | next as usize;
-        // 25%
-        let rule = rules[key];
-        // 18%
-        next_polymer.push(current);
-        // 18%
-        next_polymer.push(rule);
-
-    }
-
-    let last_char = polymer[polymer.len()-1];
-    next_polymer.push(last_char);
-
-    next_polymer
 }
