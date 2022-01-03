e78bff31cf5d7e37aceaf7a93bd2da855a34bcc1
Halvor Smedås <stektpotet@gmail.com>
2022-01-03 14:23:19 +0100

    Add solution to test.sh

--- 2021/day09/test.sh
+++ 2021/day09/test.sh
@@ -4,4 +4,5 @@ set -euo pipefail
 D=$(dirname $(realpath $0))
 
 echo "# Day 9: Smoke Basin ------------------------------------------"
-$D/../../lang/rust.sh   "$D/solutions/arxcis.rs"  "$D/io/*"
+$D/../../lang/rust.sh   "$D/solutions/arxcis.rs"      "$D/io/*"
+$D/../../lang/python.sh "$D/solutions/stektpotet.py"  "$D/io/*"
