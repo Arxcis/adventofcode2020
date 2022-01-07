fd76b2f7ba70b50aa11014f2a1dbe03dab496cbc
Halvor <stektpotet@gmail.com>
2022-01-07 14:50:11 +0100

    Solve Day11 in python (#161)

--- 2021/day11/test.sh
+++ 2021/day11/test.sh
@@ -4,5 +4,6 @@ set -euo pipefail
 D=$(dirname $(realpath $0))
 
 echo "# Day 11: Dumbo Octopus ---------------------------------------"
-$D/../../lang/rust.sh   "$D/solutions/arxcis.rs"  "$D/io/*"
+$D/../../lang/rust.sh   "$D/solutions/arxcis.rs"      "$D/io/*"
+$D/../../lang/python.sh "$D/solutions/stektpotet.py"  "$D/io/*"
 
